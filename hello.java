ca460928e3c54533a558652c9062efdaa25792fa
pradeep1765-web <68224249+pradeep1765-web@users.noreply.github.com>
2023-05-28 12:31:59 +0530

    Update hello.java
    merged changes

--- hello.java
+++ hello.java
@@ -3,7 +3,6 @@
 class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello, India");
-        System.out.println("Hello,");
         System.out.println("Hello, Siva");		
     }
 }
