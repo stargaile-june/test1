9ec194ca74e839077ddc565a0c62dcd6a5bdd087
pradeep1765-web <68224249+pradeep1765-web@users.noreply.github.com>
2023-05-28 12:40:26 +0530

    Update hello.java
    merging changes

--- hello.java
+++ hello.java
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 class HelloWorld {
 
 	public static void main(String[] args) {
@@ -6,13 +5,4 @@ class HelloWorld {
 		System.out.println("Hello world");
 	}
 }
-=======
-// Your First Program
 
-class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, India");
-        System.out.println("Hello, Siva");		
-    }
-}
->>>>>>> 89f7d4c4d6e943c2c1ce3ea78d7aa497e8f6c6ba
