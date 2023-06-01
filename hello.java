51cf4e3e90881759a91c7041decc0a3a4b7d9b70
Pradeep Ravindran <pradeep.ravindran@gmail.com>
2023-06-02 01:11:24 +0530

    hello.java edited

--- hello.java
+++ hello.java
@@ -2,9 +2,9 @@ class HelloWorld {
 
 	public static void main(String[] args) {
 	
-		System.out.println("Hello, India");
-        	System.out.println("Hello,");
-       		System.out.println("Hello, Siva");
+	    System.out.println("Hello, India");
+            System.out.println("Hello,");
+       	    System.out.println("Hello, Siva");
 	}
 }
 
